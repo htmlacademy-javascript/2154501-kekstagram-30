b89265bb06437795d8f762960e761ec05a814296
Randell87 <randell@mail.ru>
2023-11-25 12:21:07 +0700

    Надо подкачаться

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-import {createPage } from './data.js';
+//import {createPage } from './data.js';
 //import {renderThumbnails} from './picture.js';
 import './big-picture.js';
 import { renderGallery } from './gallery.js';
 import './form.js';
+import { loadPictures } from './api.js';
+import { showErrorMessage } from './function';
 
+async function bootstrap() {
+  try {
+    const pictures = await loadPictures();
+    renderGallery(pictures);
+  } catch (error) {
+    showErrorMessage();
+  }
 
-renderGallery(createPage());
+}
+
+bootstrap();
+
