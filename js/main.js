06d9ff8d6c1b88aab1415972acd65c3869c790fa
Randell87 <randell@mail.ru>
2023-11-26 11:41:47 +0700

    пуговицы

--- js/main.js
+++ js/main.js
@@ -5,11 +5,13 @@ import { renderGallery } from './gallery.js';
 import './form.js';
 import { loadPictures } from './api.js';
 import { showErrorMessage } from './function';
+import { initFilter } from './filters.js';
 
 async function bootstrap() {
   try {
     const pictures = await loadPictures();
     renderGallery(pictures);
+    initFilter(pictures);
   } catch (error) {
     showErrorMessage();
   }
